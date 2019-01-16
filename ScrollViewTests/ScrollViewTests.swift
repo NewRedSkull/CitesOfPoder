00bc21b4ecc6e5194da50ed7b0be6c0a92a71c92
Andrii <andrii@MacBook-Pro-Andrii.local>
2019-01-16 12:33:43 +0200

    new

--- ScrollViewTests/ScrollViewTests.swift
+++ ScrollViewTests/ScrollViewTests.swift
@@ -10,18 +10,22 @@ import XCTest
 @testable import ScrollView
 
 class ScrollViewTests: XCTestCase {
-
+    var app: XCUIApplication!
+   
+    
     override func setUp() {
-        // Put setup code here. This method is called before the invocation of each test method in the class.
+        super.setUp()
+        continueAfterFailure = false
+        
+        
     }
 
     override func tearDown() {
-        // Put teardown code here. This method is called after the invocation of each test method in the class.
+       super.tearDown()
     }
 
     func testExample() {
-        // This is an example of a functional test case.
-        // Use XCTAssert and related functions to verify your tests produce the correct results.
+        XCUIApplication().buttons.staticTexts["Страшна японська музика..."].tap()
     }
 
     func testPerformanceExample() {
--- ScrollViewUITests/ScrollViewUITests.swift
+++ ScrollViewUITests/ScrollViewUITests.swift
@@ -9,26 +9,43 @@
 import XCTest
 
 class ScrollViewUITests: XCTestCase {
+    var app: XCUIApplication!
 
     override func setUp() {
-        // Put setup code here. This method is called before the invocation of each test method in the class.
-
-        // In UI tests it is usually best to stop immediately when a failure occurs.
         continueAfterFailure = false
-
-        // UI tests must launch the application that they test. Doing this in setup will make sure it happens for each test method.
-        XCUIApplication().launch()
-
-        // In UI tests it’s important to set the initial state - such as interface orientation - required for your tests before they run. The setUp method is a good place to do this.
+        app = XCUIApplication()
+        app.launch()
     }
 
     override func tearDown() {
-        // Put teardown code here. This method is called after the invocation of each test method in the class.
+        super.tearDown()
     }
 
-    func testExample() {
-        // Use recording to get started writing UI tests.
-        // Use XCTAssert and related functions to verify your tests produce the correct results.
+    func testStopButtonExisting() {
+        let scrollViewsQuery = XCUIApplication().scrollViews
+        while !scrollViewsQuery.otherElements.buttons["СТОП"].isHittable {
+            app.swipeUp()
+        }
+        XCTAssert(scrollViewsQuery.otherElements.buttons["СТОП"].isHittable)
+    }
+    
+    func testOnceMoreButton() {
+        let scrollViewsQuery = XCUIApplication().scrollViews
+        while !scrollViewsQuery.otherElements.buttons["ЩЕ РАЗ"].isHittable {
+            app.swipeUp()
+        }
+        XCTAssert(scrollViewsQuery.otherElements.buttons["ЩЕ РАЗ"].isHittable)
+    }
+    
+    func testSomethingIntersting() {
+        let scrollViewsQuery = XCUIApplication().scrollViews
+        while !scrollViewsQuery.otherElements.buttons["ЩЕ РАЗ"].isHittable {
+            app.swipeUp()
+        }
+    let screenshot = XCUIScreen.main.screenshot()
+    let attachment = XCTAttachment(screenshot: screenshot)
+    attachment.lifetime = .keepAlways
+    XCTAssert(scrollViewsQuery.otherElements.buttons["ЩЕ РАЗ"].isHittable)
     }
 
 }
